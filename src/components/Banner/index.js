9a3d1e8fe25d806fe9d1b8fa2b5ebff0ab9e3650
anavollu <anavollu@id.uff.br>
2022-10-22 08:35:37 -0300

    feat: create banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,25 @@
+import { React } from "react";
+import "./index.css";
+function Banner() {
+  return (
+    <header className="banner">
+			<div className="logo-search-wrapper">
+				<img className="logo" src="/assets/logo.svg" alt="Logo" />
+				<div className="search-wrapper">
+					<p className="input-label">Search for a Pokémon by name or number</p>
+					<div className="search-bar">
+						<input className="input-search" type="search"></input>
+						<img className="search-icon" src="/assets/search-icon.svg" alt="Botão de pesquisar" />
+					</div>
+				</div>
+			</div>
+      <img
+        className="pokemons-img"
+        src="/assets/pokemons-banner.svg"
+        alt="Logo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
